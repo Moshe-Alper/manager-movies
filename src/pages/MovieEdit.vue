c5149c96a7e8edbedda98aa2d78dfb0f47ca2b0f
Moshik Alper <moshikalper@gmail.com>
2025-04-22 15:37:55 +0300

    Add more fields to edit

--- src/pages/MovieEdit.vue
+++ src/pages/MovieEdit.vue
@@ -1,37 +1,74 @@
 <template>
-  <h1>Edit</h1>
-  <form @submit.prevent="onSave" v-if="movie" class="movie-edit">
-    <input v-model="movie.title" type="text">
-    <div class="actions">
-      <RouterLink to="/movie"><button>Cancel</button></RouterLink>
-      <button type="submit">Save</button>
-    </div>
-  </form>
+  <form v-if="movie" @submit.prevent="onSave" class="movie-edit">
+      <div class="field">
+          <label>title</label>
+          <input v-model="movie.title" type="text" placeholder="title">
+      </div>
+      <div class="field">
+          <label>director</label>
+          <input v-model="movie.director" type="text" placeholder="director">
+      </div>
+      <div class="field">
+          <label>genre</label>
+          <input v-model="movie.genre" type="text" placeholder="genre">
+      </div>
+      <div class="field">
+          <label>running time</label>
+          <input v-model="movie.runningTime" type="number" placeholder="running time">
+      </div>
+      <div class="field">
+          <label>poster url</label>
+          <input v-model="movie.posterUrl" type="text" placeholder="poster url">
+      </div>
+      <button type="button" @click="getPoster">Get Poster</button>
+      <div class="field">
+          <label>actors</label>
+          <input v-bind:value="movie.actors" type="text" placeholder="poster url">
+      </div>
+      <button type="button" @click="editActors">Edit Actors</button>
 
+      <div class="actions">
+          <button>Save</button>
+          <RouterLink to="/movie"><button type="button">Cancel</button></RouterLink>
+      </div>
+  </form>
 </template>
 
 <script>
-import { movieService } from '@/services/movie.service'
+import { movieService } from '@/services/movie.service';
+
 export default {
   data() {
-    return {
-      movie: null,
-    }
+      return {
+          movie: null,
+      }
   },
   methods: {
-    async onSave() {
-      await movieService.save(this.movie)
-      this.$router.push('/movie')
-    }
-  },
-  async created() {
-    const { id: movieId } = this.$route.params
-    if (movieId) {
-      this.movie = await movieService.get(movieId)
-    }
-    this.movie = movieService.getEmptyMovie()
-  }
+      async onSave() {
+          await movieService.save(this.movie)
+          this.$router.push('/movie')
+      },
+      async getPoster() {
+          const posterUrl = await movieService.getMoviePoster(this.movie.title)
+          this.movie.posterUrl = posterUrl
+      },
+      editActors() {
+          const promptTxt = this.movie.actors.join(', ')
+          const actors = prompt('Actors:', promptTxt)
+          if (!actors) return
 
+          this.movie.actors = actors.split(',').map(actor => actor.trim())
+      }
+  },  
+  async created() {
+      const { id: movieId } = this.$route.params
+
+      if (movieId) {
+          this.movie = await movieService.get(movieId)
+      } else {
+          this.movie = movieService.getEmptyMovie()
+      }
+  }
 }
 </script>
 
@@ -39,12 +76,46 @@ export default {
 .movie-edit {
   display: grid;
   justify-items: start;
-  gap: 10px;
-  padding: 10px;
-  background-color: lightblue;
+  justify-content: center;
+  gap: 20px 30px;
 
-  .actions {
-    justify-self: end;
+  width: max-content;
+
+  padding: 20px 40px;
+  background-color: lightslategray;
+  margin-block-start: 2rem;
+  margin-inline: auto;
+
+  .field {
+      position: relative;
+
+      label {
+          position: absolute;
+          top: -.7em;
+          left: 0;
+
+          color: whitesmoke;
+          font-size: .7em;
+      }
   }
+
+  input {
+      margin-block-start: .5em;
+      &::placeholder {
+          color: lightgray;
+      }
+  }
+
+  .field:nth-child(even) {
+      grid-column: 2;
+  }
+}
+.actions {
+  display: flex;
+  gap: .3em;
+  grid-column: 1 / 3;
+
+  justify-self: end;
+  margin-block-start: 2rem;
 }
 </style>
